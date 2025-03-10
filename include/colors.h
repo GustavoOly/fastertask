d3fa888b66d9565a2b47f3345309376e6660e6b2
Gustavo Oly <gustavooly.dev@gmail.com>
2025-03-09 22:20:16 -0300

    feat: colors headers

---
+++ include/colors.h
@@ -0,0 +1,40 @@
+#ifndef COLORS_H
+#define COLORS_H
+
+// Código ANSI para resetar a cor
+#define RESET "\x1B[0m"
+
+// Cores básicas
+#define BLACK "\x1B[30m"
+#define RED "\x1B[31m"
+#define GREEN "\x1B[32m"
+#define YELLOW "\x1B[33m"
+#define BLUE "\x1B[34m"
+#define MAGENTA "\x1B[35m"
+#define CYAN "\x1B[36m"
+#define WHITE "\x1B[37m"
+
+// Cores em negrito
+#define BBLACK "\x1B[1;30m"
+#define BRED "\x1B[1;31m"
+#define BGREEN "\x1B[1;32m"
+#define BYELLOW "\x1B[1;33m"
+#define BBLUE "\x1B[1;34m"
+#define BMAGENTA "\x1B[1;35m"
+#define BCYAN "\x1B[1;36m"
+#define BWHITE "\x1B[1;37m"
+
+// Cores de fundo
+#define BG_BLACK "\x1B[40m"
+#define BG_RED "\x1B[41m"
+#define BG_GREEN "\x1B[42m"
+#define BG_YELLOW "\x1B[43m"
+#define BG_BLUE "\x1B[44m"
+#define BG_MAGENTA "\x1B[45m"
+#define BG_CYAN "\x1B[46m"
+#define BG_WHITE "\x1B[47m"
+
+// Resetando cores para evitar interferência
+#define RESET_COLOR "\x1B[0m"
+
+#endif
---
+++ main.c
@@ -0,0 +1,23 @@
+/***************************************************************************
+ *                                                                         *
+ *                           Gerenciador de Tarefas                        *
+ *                                                                         *
+ ***************************************************************************
+ *                                                                         *
+ *  Autor: Gustavo Oly                                                     *
+ *  Data de Criação: 09/03/2025                                            *
+ *                                                                         *
+ *  Descrição:                                                             *
+ *  Este programa permite ao usuário registrar e listar tarefas.           *
+ *  As tarefas são armazenadas em um arquivo de texto e possuem            *
+ *  um identificador, uma descrição e um status (TODO, DOING, DONE).       *
+ *                                                                         *
+ ***************************************************************************/
+
+#include "./include/navegation.h"
+
+int main()
+{
+    navegation();
+    return 0;
+}
